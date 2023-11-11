b4e43fb15cc9d3c8bc7b8c20dff82a0d83c9a4bc
Marvell2377 <143757231+Marvell2377@users.noreply.github.com>
2023-11-11 02:44:19 +0100

    Create Learn Maffy.js
    Learn Maffy.js, it's attributes and functions 
    https://godsonebube.graphy.com/courses/Learn-Maffyjs-and-how-to-make-use-of-it-to-create-extremely-fun-games-65457fd3e4b0e826e0f7f3a2

---
+++ Learn Maffy.js
@@ -0,0 +1,2 @@
+Maffy.js is a script in script code which erases the use of creating css file and html tags to learn copy and paste link on browser
+https://godsonebube.graphy.com/courses/Learn-Maffyjs-and-how-to-make-use-of-it-to-create-extremely-fun-games-65457fd3e4b0e826e0f7f3a2
